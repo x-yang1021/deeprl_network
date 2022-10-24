4cbe3fde5979e9ed348040394ee57bd4adcda82e
x-yang1021 <xiangmin.yang1021@gmail.com>
2022-10-24 09:08:37 +0100

    little change

--- envs/atsc_env.py
+++ envs/atsc_env.py
@@ -368,6 +368,8 @@ class TrafficSimulator:
 
     def _init_sim(self, seed, gui=False):
         sumocfg_file = self._init_sim_config(seed)
+        teleport_range = np.linspace(900, 1200, 7)
+        teleport_time = np.random.choice(teleport_range)
         if gui:
             app = 'sumo-gui'
         else:
@@ -376,7 +378,7 @@ class TrafficSimulator:
         command += ['--seed', str(seed)]
         command += ['--remote-port', str(self.port)]
         command += ['--no-step-log', 'True']
-        command += ['--time-to-teleport', '600'] # long teleport for safety
+        command += ['--time-to-teleport', '%d' % teleport_time]  # long teleport for safety
         command += ['--no-warnings', 'True']
         command += ['--duration-log.disable', 'True']
         # collect trip info if necessary
