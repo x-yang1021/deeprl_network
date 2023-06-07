find out where scaling went wrong

envs/atsc_env.py
@@ -469,9 +469,11 @@ class TrafficSimulator:
                 reward = - queue - wait
             reward_queue.append(reward)
         reward_queue = np.array(reward_queue)
+        print("unscaled index", reward_queue)
         reward_queue = reward_queue.reshape(-1,1)
         scaler.fit(reward_queue)
         reward_queue = scaler.transform(reward_queue)
+        print("scaled index", reward_queue)
         reward_avg_queue = - np.mean(reward_queue)
         reward_std_queue = - np.std(reward_queue)
         # risk_inices = np.array(self.get_risk_index())
@@ -491,7 +493,6 @@ class TrafficSimulator:
         for key in edge_veh:
             veh_pos = {}
             veh_forward = {}
-            veh_acc = {}
             for veh in edge_veh[key]:
                 veh_pos[veh] = self.sim.vehicle.getPosition(veh)
                 veh_forward[veh] = self.sim.vehicle.getAngle(veh)
@@ -545,11 +546,14 @@ class TrafficSimulator:
                     right_speed = self.sim.vehicle.getSpeed(right)
                     ttc_right = self.ttc(-right_dis, ego_speed, right_speed, veh_width)
                 ttc = min(ttc_front,ttc_right,ttc_rear,ttc_left)
+                print("ttc", ttc)
                 reward_safety_index.append(float(ttc))
         reward_safety_index = np.array(reward_safety_index)
+        print("unscaled index", reward_safety_index)
         reward_safety_index = reward_safety_index.reshape(-1,1)
         scaler.fit(reward_safety_index)
         reward_safety_index = scaler.transform(reward_safety_index)
+        print("scaled index", reward_safety_index)
         reward_safety_index = np.mean(reward_safety_index)
         rewards = reward_safety_index + reward_std_queue + reward_avg_queue
         # for node_name in self.node_names:

utils.py
@@ -240,7 +240,7 @@ class Trainer():
             self.cur_step = 0
             self.episode_rewards = []
             num_accident = np.random.choice(stats.poisson.rvs(mu=4, size=720))
-            self.accident_step = np.random.choice(720,num_accident)
+            self.accident_step = np.random.choice(720, num_accident)
             while True:
                 ob, done, R = self.explore(ob, done)
                 dt = self.env.T - self.cur_step