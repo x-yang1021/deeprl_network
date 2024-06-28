334e5aaacd0dcd57f0bcf6d32a499701ebbe8a9a
x-yang1021 <xiangmin.yang1021@gmail.com>
2024-06-28 18:01:02 +0100

    add atsc benchmark

--- Extract data.py
+++ Extract data.py
@@ -10,14 +10,13 @@ def extract_rewards(file_path, tag):
     return rewards
 
 # Extract rewards
-proposed_02_rewards = extract_rewards('./2/events.out.tfevents.1692214720.cv-iits-ra02','std_queue')
-proposed_08_rewards = extract_rewards('./8/events.out.tfevents.1691862730.dsw-28692-b46d9b4f8-td46k','std_queue')
+proposed_04_rewards = extract_rewards('./4/events.out.tfevents.cv-iits-ra02','train_reward')
 
 # Save the last 30 episodes to CSV
-with open('proposed_02_last30_resilience.csv', 'w', newline='') as file:
+with open('proposed_02_last30.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(proposed_02_rewards[-30:])
+    writer.writerow(proposed_04_rewards[-30:])
 
-with open('proposed_08_last30_resilience.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(proposed_08_rewards[-30:])
+# with open('proposed_08_last30_resilience.csv', 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerow(proposed_08_rewards[-30:])
